353de4494af6eb896d8d32c9ce6f1229f2d743e5
sidharthvnair02 <sidharthvnair02@gmail.com>
2023-04-01 16:26:36 +0530

    hotels

--- server/index.js
+++ server/index.js
@@ -6,12 +6,16 @@ const port = process.env.PORT || 8989
 // Calling the connect file to connect with the atlas database
 const conn = require('./connect.js')
 const userRouter = require('./routes/user_rou')
+const hotelsRoute = require('./routes/hotels.js')
+
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
 app.use('/user',userRouter);
+app.use("/server/hotels",hotelsRoute);
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${8989}`)
 })
+
